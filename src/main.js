b5bfe4d3b86671714e737999ba51be622f11a495
oluwa Ibrahim <mj9762651@gmail.com>
2025-07-12 02:32:52 +0100

    add and styled footer with copyright and text content

--- src/main.js
+++ src/main.js
@@ -1,1 +1,9 @@
-import './style.css';
+import "./style.css";
+
+// const header = document.querySelector("header");
+
+// window.addEventListener("scroll", () => {
+//   window.scrollY > 10
+//     ? header.classList.add("show-bg")
+//     : header.classList.remove("show-bg");
+// });
